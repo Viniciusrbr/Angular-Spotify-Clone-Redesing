f9a66c754200c921b93f59a5ea3af7265f00d10f
Viniciusrbr <viniciusgoes159@gmail.com>
2023-02-17 10:18:20 -0300

    parte de login finalizada

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { SpotifyConfiguration } from 'src/environments/environment';
 import Spotify from 'spotify-web-api-js';
+import { IUsuario } from '../interfaces/IUsuario';
+import { SpotifyUserParaUsuario } from '../Common/spotifyHelper';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,38 @@ import Spotify from 'spotify-web-api-js';
 export class SpotifyService {
 
   spotifyApi: Spotify.SpotifyWebApiJs = null;
+  usuario: IUsuario;
 
   constructor() {
     this.spotifyApi = new Spotify();
-   }
+  }
 
-  oberUrlLogin(){
+  async inicializarUsuario() {
+    if (!!this.usuario)
+      return true;
+
+    const token = localStorage.getItem('token');
+
+    if (!token)
+      return false;
+
+    try {
+
+      this.definirAccessToken(token);
+      await this.obterSpotifyUusuario();
+      return !!this.usuario;
+
+    } catch (ex) {
+      return false;
+    }
+  }
+
+  async obterSpotifyUusuario() {
+    const useInfo = await this.spotifyApi.getMe();
+    this.usuario = SpotifyUserParaUsuario(useInfo);
+  }
+
+  oberUrlLogin() {
     const authEndpoint = `${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUri = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
@@ -23,15 +51,15 @@ export class SpotifyService {
     return authEndpoint + clientId + redirectUri + scopes + responseType;
   }
 
-  obterTokenUrlCallback(){
-    if(!window.location.hash)
+  obterTokenUrlCallback() {
+    if (!window.location.hash)
       return '';
-    
+
     const params = window.location.hash.substring(1).split('&');
     return params[0].split('=')[1];
   }
 
-  definirAccessToken(token:string){
+  definirAccessToken(token: string) {
     this.spotifyApi.setAccessToken(token);
     localStorage.setItem('token', token);
   }
