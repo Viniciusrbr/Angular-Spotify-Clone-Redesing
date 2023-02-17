6ba5b87a70c85bd4307ce18e1d74669d8d1e8cb3
Viniciusrbr <viniciusgoes159@gmail.com>
2023-02-16 22:14:39 -0300

    tratamento do token

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@angular/core';
 import { SpotifyConfiguration } from 'src/environments/environment';
+import Spotify from 'spotify-web-api-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor() { }
+  spotifyApi: Spotify.SpotifyWebApiJs = null;
+
+  constructor() {
+    this.spotifyApi = new Spotify();
+   }
 
   oberUrlLogin(){
     const authEndpoint = `${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUri = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
-    const scopes = `scopes=${SpotifyConfiguration.scopes.join("%20")}&`; /** para concatenar o Array de Strings */
+    const scopes = `scope=${SpotifyConfiguration.scopes.join("%20")}&`; /** para concatenar o Array de Strings */
     const responseType = `response_type=token&show_dialog=true`;
 
     return authEndpoint + clientId + redirectUri + scopes + responseType;
+  }
+
+  obterTokenUrlCallback(){
+    if(!window.location.hash)
+      return '';
+    
+    const params = window.location.hash.substring(1).split('&');
+    return params[0].split('=')[1];
+  }
+
+  definirAccessToken(token:string){
+    this.spotifyApi.setAccessToken(token);
+    localStorage.setItem('token', token);
   }
 }
